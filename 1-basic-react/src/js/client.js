a3460445b018497704695fcb5ab02a8c3f25cd20
Elliot Plant <elliotaplant@gmail.com>
2018-05-01 17:16:07 -0700

    Basic store

--- 1-basic-react/src/js/client.js
+++ 1-basic-react/src/js/client.js
@@ -1,7 +1,23 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import {createStore} from 'redux';
 
-import Layout from "./components/Layout";
+const reducer = (state, action) => {
+  if (action.type === 'INC') {
+    return state + action.payload;
+  }
+  if (action.type === 'DEC') {
+    return state - action.payload;
+  }
+  return state;
+}
 
-const app = document.getElementById('app');
-ReactDOM.render(<Layout/>, app);
+const store = createStore(reducer, 0);
+
+store.subscribe(() => {
+  console.log('store changed!', store.getState());
+})
+
+store.dispatch({type: 'INC', payload: 1});
+store.dispatch({type: 'INC', payload: 12});
+store.dispatch({type: 'INC', payload: 22});
+store.dispatch({type: 'DEC', payload: 100});
+store.dispatch({type: 'INC', payload: 123});
