eeac22142f6971b30d66065c3f3bc87b1c40cb85
Elliot Plant <elliotaplant@gmail.com>
2018-05-03 16:31:18 -0700

    cp

--- 1-basic-react/src/js/client.js
+++ 1-basic-react/src/js/client.js
@@ -1,14 +1,15 @@
 import {applyMiddleware, createStore} from 'redux';
 import logger from 'redux-logger';
+import thunk from 'redux-thunk';
 
 const reducer = (state={}, action) => {
-  if (action.type === 'INC') {
-    return state + action.payload;
-  }
-  if (action.type === 'DEC') {
-    return state - action.payload;
-  }
   return state;
 }
 
-store.dispatch({ type: 'FOO' });
+const middleware = applyMiddleware(thunk, logger);
+const store = createStore(reducer, middleware);
+
+store.dispatch(dispatch => {
+  dispatch({ type: 'LOADING' })
+  setTimeout(() => dispatch({ type: 'DONE'}), 100)
+});
